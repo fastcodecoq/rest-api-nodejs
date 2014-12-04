2c18800d627c231815619d8befc77f8743fb7d49
Gomosoft <jgomez@soluntech.com>
2014-12-04 15:58:24 -0500

    optimizing model.basejs

--- api/models/model.base.js
+++ api/models/model.base.js
@@ -2,12 +2,27 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+// Load required packages
+var timestamps = require('mongoose-timestamp');
+var location = require('./plugins/location');
+var candidate_data = require('./plugins/candidate_data');
+var email = require('./plugins/email');
 
 
-var Schema = new Schema({
-	  created_date : {type: Date},
-	  updated_date : {type: Date, default: Date.now}
+var nameSchema = new Schema({
+
 });
+
+
+
+//add createdAt, updatedAt fields
+UsuerioSchema.plugin(timestamps);
+//add location field
+UsuarioSchema.plugin(location);
+//add candidate data
+UsuarioSchema.plugin(candidate_data);
+//add email
+UsuarioSchema.plugin(email);
 
 
 module.exports = new mongoose.model('model', Schema); 

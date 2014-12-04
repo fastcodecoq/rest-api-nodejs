667e68d5e22f57dc37f2082bd57b685d44b8fe60
Gomosoft <jgomez@soluntech.com>
2014-12-04 17:42:09 -0500

    ctrl/usuario.js

--- api/controllers/usuario.js
+++ api/controllers/usuario.js
@@ -34,7 +34,7 @@ var ctrlUsuario = function (server) {
 // Use the Usuario model to find a specific usuario
      var query = {};
 
-     !req.body.userid || (query._id = mongoose.Types.ObjectId(req.body.userid))    
+     !req.body.userid || (query._id = mongoose.Types.ObjectId(req.params.userid))    
 
 
 // Use the Usuario model to find all usuario

42304dbcab821da7d75acef932aee8f840d0fdcc
Gomosoft <jgomez@soluntech.com>
2014-12-05 11:37:30 -0500

    + controller/candidate

--- api/controllers/all.js
+++ api/controllers/all.js
@@ -3,5 +3,6 @@ module.exports = exports = function(server){
 	     require('./usuario.js')(server);	     
 	     require('./empresa.js')(server);	     
 	     require('./roles.js')(server);	     
+	     require('./candidate.js')(server);	     
 
 }

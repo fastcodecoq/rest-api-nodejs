fbb3078da5237251952bb9af4a3dceb155af8bae
Gomosoft <jgomez@soluntech.com>
2014-12-05 14:35:57 -0500

    + controller/orden_servicio

--- api/controllers/all.js
+++ api/controllers/all.js
@@ -2,9 +2,11 @@ module.exports = exports = function(server){
 	  
 	var fs = require('fs');
 	var files = fs.readdirSync('./');
-	
-	for (x in files)
-		require(files[x]);
+
+		for (x in files)
+		 if(!files[x].match('gitignore|base'))
+   		     require(files[x])(server);
+
 
 
 }
--- api/models/plugins/all.js
+++ api/models/plugins/all.js
@@ -2,4 +2,5 @@ var fs = require('fs');
 var files = fs.readdirSync('./');
 
 	for (x in files)
-		require(files[x]);
+		if(!files[x].match('gitignore|base'))
+		    require(files[x]);

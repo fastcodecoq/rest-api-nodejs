1c5bc0cdb7107327c4496274680020b0ee9420ba
Gomosoft <jgomez@soluntech.com>
2014-12-05 09:59:40 -0500

    + controller/role

--- api/models/plugins/empresa.js
+++ api/models/plugins/empresa.js
@@ -1,12 +1,14 @@
 
   module.exports = exports = function empresaPlugin(schema, options) {
 
+  ObjectId = require('mongoose').Schema.Types.ObjectId;
+
   options || (options = {});
 
   schema.add({
   	_user_id: {
     
-    type: Schema.Types.ObjectId,
+    type: ObjectId,
     required: true,
     ref: 'Empresa'
        

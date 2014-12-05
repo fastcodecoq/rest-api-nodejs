4a56106f941429449b975bcb2ab32de6a5f0b3ec
Gomosoft <jgomez@soluntech.com>
2014-12-05 17:09:40 -0500

    + controllers/perfil_laboral

--- api/models/perfil_laboral.js
+++ api/models/perfil_laboral.js
@@ -9,26 +9,28 @@ var location = require('./plugins/location');
 
 
 var perfil_laboralSchema = new Schema({
-	  name : { type : String, trim : true},
-	  area : [{ type : String, trim : true, lowercase:true}],
-	  profesion: { type : String, trim : true, lowercase:true},
+	  name : { type : String, trim : true, required: true},
+	  area : { type : [{ type : String, trim : true, lowercase:true}], required: true},
+	  profesion: { type : String, trim : true, lowercase:true, required: true},
 	  experience: { type : String, trim : true, lowercase:true},
 	  salary_range: { type : String, trim : true},
-	  functions: { type : String, trim : true, lowercase:true},
+	  functions: { type : String, trim : true, lowercase:true, required: true},
 	  priorities: { type : String, trim : true, lowercase:true},
 	  results_expected: { type : String, trim : true, lowercase:true},
 	  english_level: { type : String, trim : true},
-	  academic_level: { type : String, trim : true, lowercase:true},
-	  academic_title: { type : String, trim : true, lowercase:true},
+	  academic_level: { type : String, trim : true, lowercase:true, required: true},
+	  academic_title: { type : String, trim : true, lowercase:true, required: true},
 	  genre: { type : String, trim : true, lowercase:true},
 	  notes: { type : String, trim : true},
+	  industry_sector : { type : String, trim : true, required: true},
 	  project_presentation: { type : String, trim : true},
+	  personal_caracteristics: { type : String, trim : true},
 	  min_experience_years: {type: Number},
-	  max_experience_years: {type: Number},
+	  max_experience_years: {type: Number},	  
 	  status: {type: Number},	  
 	  contract_type: {type: Number},	  
 	  _candidates : [{type: Schema.Types.ObjectId, ref : 'Candidate'}],
-	  _orden_servicio : {type : Schema.Types.ObjectId, ref: 'OrdenServicio'}
+	  _orden_servicio : {type : Schema.Types.ObjectId, ref: 'OrdenServicio', required: true}
 });
 
 

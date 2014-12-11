349f8d0f411e788e4bc22868182c5c71bae799b5
Gomosoft <jgomez@soluntech.com>
2014-12-11 16:35:27 -0500

    +- README

--- api/models/plugins/location.js
+++ api/models/plugins/location.js
@@ -3,22 +3,7 @@ module.exports = exports = function locationPlugin(schema, options) {
   options || (options = {});
 
   schema.add({
-    location: {
-      address: {
-        type: String,
-        trim: true
-      },
-      city: String,
-      country: String,
-      lat: {
-        type: Number,
-        match: [/^(\-?\d+(\.\d+)?)+$/, 'invalid latitude']
-      },
-      lng: {
-        type: Number,
-        match: [/^(\-?\d+(\.\d+)?)$/, 'invalid longitude']
-      }
-    }
+    location: Object
   });
 
 };

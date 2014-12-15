e3d845ca60d955d718fa8f3431b2fdc65cc16437
Gomosoft <jgomez@soluntech.com>
2014-12-15 15:02:14 -0500

    empresa +-

--- api/models/empresa.js
+++ api/models/empresa.js
@@ -25,7 +25,7 @@ var EmpresaSchema = new mongoose.Schema({
     type: Number    
   },
   contact : [{ type : Schema.Types.ObjectId , ref : 'Usuario'}],
-  competences :[String]
+  competences :[Object]
 });
 
 

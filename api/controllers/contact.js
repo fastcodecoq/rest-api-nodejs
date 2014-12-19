e71e17a517c93f5d79159e1d899d5097adafccda
Gomosoft <jgomez@soluntech.com>
2014-12-19 10:05:32 -0500

    contact

--- api/controllers/contact.js
+++ api/controllers/contact.js
@@ -81,6 +81,7 @@ module.exports = exports = function(server){
   
   server.get(global.apiBaseUri + '/contacts/empresa/:empresaid', get);
   server.get(global.apiBaseUri + '/contacts/:contactid', get);
+  server.get(global.apiBaseUri + '/contacts', get);
   server.post(global.apiBaseUri + '/contacts', post);
 
 

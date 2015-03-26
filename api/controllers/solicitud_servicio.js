7cc13f0260c7a0334807f7b073709bc457bb5acb
Gomosoft <jgomez@soluntech.com>
2015-03-26 05:23:37 -0500

    latest

--- api/controllers/solicitud_servicio.js
+++ api/controllers/solicitud_servicio.js
@@ -65,7 +65,7 @@ var ctrlSolicitud_servicio = function (server) {
     .populate('_empresa')
     .populate('_contacto')
     .populate('_responsable')
-    .populate('_cargo')
+    //.populate('_cargo')
     .populate('_modelo_competencia')
     .exec(function (err, solicitud_servicios) {
       if (err) {
